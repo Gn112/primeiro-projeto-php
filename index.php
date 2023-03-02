aca517dfead99f18461bf14810ab1bb00de6a52b
Gabriel Siriani <50271636+Gn112@users.noreply.github.com>
2023-03-02 09:49:14 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Primeiro Projeto - Gabriel Siriani</title>
+
+    <style>
+        h2 {
+            font-family: 'Verdana';
+            font-style: normal;
+            color: white;
+        }
+
+        body{
+            background-color: LightPink; 
+        }
+    </style>
+
+</head>
+<body>
+    <h1> <?php echo "Ola Mundo"?> </h1>
+
+    <h2> 3 + 5 = <?php echo 3 + 5?> </h2>
+    <h2> 1 - 2 = <?php echo 1 - 2?> </h2>
+    <h2> 11 * 11 = <?php echo 11 * 11?> </h2>
+    <h2> 4 / 2 = <?php echo 4 / 2?> </h2>
+</body>
+</html>
